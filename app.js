ecb78143b05d59c951b247708b8219bb311e61f7
Richard Lee <rblee88@gmail.com>
2015-08-07 19:45:14 -0500

    Add player that earns points if milanos land in the mouth

--- app.js
+++ app.js
@@ -9,16 +9,43 @@ var Milano = function (state) {
   Phaser.Sprite.call(this, state.game, 0, 0, 'milano');
   this.alive = false;
   this.exists = false;
+  this.anchor.setTo(0.5, 0.5);
+  this.rotation = Math.PI/2;
 }
 
 Milano.prototype = Object.create(Phaser.Sprite.prototype);
 Milano.prototype.constructor = Milano;
 
+var Player = function (state) {
+  this.gameState = state;
+  this.game = state.game;
+  this.moveSpeed = 250;
+  Phaser.Sprite.call(this, this.game, 400, 550, 'player');
+  this.game.add.existing(this);
+  this.game.physics.arcade.enable(this);
+
+  this.body.collideWorldBounds = true;
+  this.cursors = this.game.input.keyboard.createCursorKeys();
+}
+
+Player.prototype = Object.create(Phaser.Sprite.prototype);
+Player.prototype.constructor = Player;
+
+Player.prototype.update = function(){
+  this.body.velocity.x = 0;
+  if(this.cursors.left.isDown && this.body.x > 0){
+    this.body.velocity.x -= this.moveSpeed;
+  }
+  if(this.cursors.right.isDown && this.body.x < GAME_WIDTH-this.body.width){
+    this.body.velocity.x += this.moveSpeed;
+  }
+}
+
 
 Main.prototype = {
   preload: function(){
     this.game.stage.backgroundColor = '#71c5cf';
-    this.game.load.image('cone', 'assets/cone.png');
+    this.game.load.image('player', 'assets/openMouthFace.png');
     this.game.load.image('milano', 'assets/milano.png');
     this.cursors = game.input.keyboard.createCursorKeys();
   },
@@ -26,7 +53,7 @@ Main.prototype = {
     this.score = 0;
     this.scoreText = game.add.text(20, 20, "Score: 0", {font: "16px Arial", fill: "#FFFFFF"});
     this.game.physics.startSystem(Phaser.Physics.ARCADE);
-    this.player = game.add.sprite(300, 550, 'cone');
+    this.player = new Player(this);
     this.game.physics.arcade.enable(this.player);
     this.player.body.collideWorldBounds = true;
 
@@ -40,39 +67,23 @@ Main.prototype = {
     this.game.time.events.loop(this.game.rnd.between(250, 450), this.spawnMilano, this);
   },
   update: function(){
-    this.player.body.velocity.x = 0;
-    if(this.cursors.left.isDown && this.player.body.x > 0){
-      this.player.body.velocity.x -= 250;
-    }
-    if(this.cursors.right.isDown && this.player.body.x < GAME_WIDTH-this.player.body.width){
-      this.player.body.velocity.x += 250;
-    }
-
-    this.game.physics.arcade.overlap(this.player, this.milanos, this.milanoHitsCone, null, this);
-    this.game.physics.arcade.overlap(this.milanos, this.milanos, this.milanoHitsMilano, null, this);
+    this.game.physics.arcade.overlap(this.player, this.milanos, this.milanoHitsPlayer, null, this);
   },
   spawnMilano: function(){
     var milano = this.milanos.getFirstDead();
     milano.reset(this.game.rnd.between(0, 600), -50);
     milano.body.velocity.y = 300;
   },
-  milanoHitsCone: function(player, milano){
-    if(!milano.alreadyScored){
-      milano.alreadyScored = true;
-      milano.body.velocity.y = 0;
-      milano.body.velocity = player.body.velocity;
+  milanoHitsPlayer: function(player, milano){
+    var milanoLeftEdge = milano.body.center.x - milano.height/2; //Uses height because the milano is rotated
+    var milanoRightEdge = milano.body.center.x + milano.height/2; //Uses height because the milano is rotated
+    var playerMouthLeft = player.left + 30;
+    var playerMouthRight = player.right - 7;
+
+    if(milanoLeftEdge > playerMouthLeft && milanoRightEdge < playerMouthRight) {
       this.addPoints();
     }
-  },
-  milanoHitsMilano: function(milano1, milano2){
-    if(!milano2.alreadyScored){
-      if(milano1 != milano2){
-        milano2.alreadyScored = true;
-        milano2.body.velocity.y = 0;
-        milano2.body.velocity = milano1.body.velocity;
-        this.addPoints();
-      };
-    };
+    milano.kill();
   },
   addPoints: function(){
     this.score += 1;
